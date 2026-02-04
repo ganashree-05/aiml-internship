bbd1aabf4a06b66c9d6f12e24a0a3aa4bfa5c944
ganashree-05 <cnganashree@gmail.com>
2026-02-04 22:43:59 +0530

    Delete raw.py

--- raw.py
+++
@@ -1,7 +0,0 @@
-item_name="Laptop"
-quantity=2
-price=499.99
-in_stock=True
-print("item:", item_name,"quantity:",quantity,"price:",price,"Available:",in_stock)
-total_cost=quantity*price
-print(total_cost)
